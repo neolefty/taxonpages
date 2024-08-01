48b8f516e5d42a5c1795daa57b8b023f936910c5
Beecher Baker <wbbaker@purdue.edu>
2024-08-01 16:05:40 -0400

    add placeholder for Collectino Objects panel

---
+++ src/modules/otus/components/Panel/PanelCollectionObjects/PanelCollectionObjects.vue
@@ -0,0 +1,21 @@
+<template>
+  <VCard>
+    <ClientOnly>
+      <VSpinner
+        v-if="isLoading"
+        logo-class="w-8 h-8"
+        legend=""
+      />
+      <VCardHeader>
+        <h2 class="text-md">Collection Objects</h2>
+        <PanelDropdown panel-key="panel:collection-objects" />
+      </VCardHeader>
+    </ClientOnly>
+  </VCard>
+</template>
+
+<script setup>
+
+const isLoading = ref(false)
+</script>
+
---
+++ src/modules/otus/components/Panel/PanelCollectionObjects/main.js
@@ -0,0 +1,6 @@
+import PanelCollectionObjects from "./PanelCollectionObjects.vue";
+
+export default {
+  id: 'panel:collection-objects',
+  component: PanelCollectionObjects
+}
